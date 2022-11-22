7ed7c8ec922d5cb978d25034166e6df2c9ad83ee
overwatch <nzioki.grace@gmail.com>
2022-11-22 18:19:32 +0300

    user todo items

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,22 +9,20 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        user_id = sys.argv[1]
 
-        r1 = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
-        r2 = requests.get("https://jsonplaceholder.typicode.com/todos?userId=")
+        r1 = requests.get(f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}")
+        r2 = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}")
 
-        name = r1.json().get("name")
-        completed = 0
-        total = 0
+        EMPLOYEE_NAME = r1.json()['name']
+        NUMBER_OF_DONE_TASKS = 0
+        TOTAL_NUMBER_OF_TASKS = len(r2.json())
         done_with = []
 
-        for dict in r2.json():
-            if dict.get("completed"):
-                completed += 1
-                done_with.append(dict.get("title"))
-            total += 1
+        for dict_ in r2.json():
+            if dict_.get("completed"):
+                NUMBER_OF_DONE_TASKS += 1
+                done_with.append(dict_['title'])
 
-        print(f"Employee {name} is done with tasks({completed}/{total}):")
+        print(f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
         for task in done_with:
             print(f"\t {task}")
